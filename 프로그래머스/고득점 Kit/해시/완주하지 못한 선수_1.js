d7c3605b5fba0c42f291fc545c76519c5062a1a0
Abyul <kinsng1599@gmail.com>
2022-02-05 23:33:10 +0900

    문제 유형 : 해시 Map > 객체 > 배열 > sort & loop 순으로 효율성 테스트 속도가 빨랐습니다. 이를 테스트를 하기전 다음과 같은 사이트를 참고 했습니다. https://erim1005.tistory.com/entry/Javascript-%EC%97%90%EC%84%9C-Object%EB%A5%BC-%ED%95%B4%EC%8B%9C%EB%A7%B5%EC%B2%98%EB%9F%BC-%EC%82%AC%EC%9A%A9%ED%95%98%EC%A7%80-%EB%A7%88%EC%84%B8%EC%9A%94

--- 프로그래머스/고득점 Kit/해시/완주하지 못한 선수_1.js
+++ 프로그래머스/고득점 Kit/해시/완주하지 못한 선수_1.js
@@ -1,22 +1,130 @@
+// sort & loop 를 사용한 방법
+/*
+정확성  테스트
+테스트 1 〉	통과 (0.05ms, 30.3MB)
+테스트 2 〉	통과 (0.08ms, 30.2MB)
+테스트 3 〉	통과 (0.30ms, 30.2MB)
+테스트 4 〉	통과 (0.59ms, 30.5MB)
+테스트 5 〉	통과 (0.62ms, 30.4MB)
+효율성  테스트
+테스트 1 〉	통과 (46.19ms, 41MB)
+테스트 2 〉	통과 (77.15ms, 48.1MB)
+테스트 3 〉	통과 (94.38ms, 52.5MB)
+테스트 4 〉	통과 (95.84ms, 54.6MB)
+테스트 5 〉	통과 (103.65ms, 53.4MB)
+*/
+// function solution(participant, completion) {
+//   participant.sort();
+//   completion.sort();
+//   for (let i = 0; i < participant.length; i++) {
+//     if (participant[i] !== completion[i]) return participant[i];
+//   }
+// }
+
+// Map를 이용한 방법
+/*
+정확성  테스트
+테스트 1 〉	통과 (0.12ms, 30.2MB)
+테스트 2 〉	통과 (0.10ms, 30.4MB)
+테스트 3 〉	통과 (0.45ms, 30.2MB)
+테스트 4 〉	통과 (0.43ms, 30.2MB)
+테스트 5 〉	통과 (0.36ms, 30.3MB)
+효율성  테스트
+테스트 1 〉	통과 (19.99ms, 45.6MB)
+테스트 2 〉	통과 (35.49ms, 56.7MB)
+테스트 3 〉	통과 (36.62ms, 60.1MB)
+테스트 4 〉	통과 (41.05ms, 59.5MB)
+테스트 5 〉	통과 (35.01ms, 59.6MB)
+*/
+// function solution(participant, completion) {
+//   let answerMap = new Map();
+//   participant.map((el) => {
+//     if (answerMap.get(el)) answerMap.set(el, answerMap.get(el) + 1);
+//     else answerMap.set(el, 1);
+//   });
+
+//   completion.map((el) => {
+//     if (answerMap.get(el)) answerMap.set(el, answerMap.get(el) - 1);
+//   });
+
+//   for (let entry of answerMap) {
+//     if (entry[1] >= 1) return entry[0];
+//   }
+// }
+
+// 객체를 이용한 방법
+/*
+정확성  테스트
+테스트 1 〉	통과 (0.11ms, 30.3MB)
+테스트 2 〉	통과 (0.10ms, 30.3MB)
+테스트 3 〉	통과 (0.45ms, 30.2MB)
+테스트 4 〉	통과 (0.45ms, 30.2MB)
+테스트 5 〉	통과 (0.55ms, 30.2MB)
+효율성  테스트
+테스트 1 〉	통과 (25.24ms, 48.1MB)
+테스트 2 〉	통과 (38.33ms, 52.9MB)
+테스트 3 〉	통과 (45.24ms, 56.9MB)
+테스트 4 〉	통과 (70.64ms, 65.5MB)
+테스트 5 〉	통과 (61.63ms, 65.8MB)
+*/
+// function solution(participant, completion) {
+//   let obj = {};
+//   participant.map((el) => {
+//     if (obj[el]) obj[el] += 1;
+//     else obj[el] = 1;
+//   });
+
+//   completion.map((el) => {
+//     if (obj[el]) obj[el] -= 1;
+//   });
+
+//   for (let key in obj) {
+//     if (obj[key] >= 1) return key;
+//   }
+// }
+
+// 배열을 이용한 방법
+/*
+정확성  테스트
+테스트 1 〉	통과 (0.24ms, 30.3MB)
+테스트 2 〉	통과 (0.31ms, 30.3MB)
+테스트 3 〉	통과 (0.47ms, 30.3MB)
+테스트 4 〉	통과 (0.74ms, 30.2MB)
+테스트 5 〉	통과 (0.54ms, 30.4MB)
+효율성  테스트
+테스트 1 〉	통과 (33.37ms, 48.1MB)
+테스트 2 〉	통과 (46.15ms, 52.8MB)
+테스트 3 〉	통과 (48.48ms, 56.2MB)
+테스트 4 〉	통과 (75.87ms, 66.2MB)
+테스트 5 〉	통과 (73.47ms, 66.6MB)
+*/
 function solution(participant, completion) {
-  participant.sort();
-  completion.sort();
-  for (let i = 0; i < participant.length; i++) {
-    if (participant[i] !== completion[i]) return participant[i];
+  let arr = [];
+  participant.map((el) => {
+    if (arr[el]) arr[el] += 1;
+    else arr[el] = 1;
+  });
+
+  completion.map((el) => {
+    if (arr[el]) arr[el] -= 1;
+  });
+
+  for (let key in arr) {
+    if (arr[key] >= 1) return key;
   }
 }
 
 let participant = ['leo', 'kiki', 'eden'];
 let completion = ['eden', 'kiki'];
 let output = solution(participant, completion);
-// console.log('output : ', output);
+console.log('output : ', output);
 
 // participant = ['marina', 'josipa', 'nikola', 'vinko', 'filipa'];
 // completion = ['josipa', 'filipa', 'marina', 'nikola'];
 // output = solution(participant, completion);
 // console.log('output : ', output);
 
-participant = ['mislav', 'stanko', 'mislav', 'ana'];
-completion = ['stanko', 'ana', 'mislav'];
+participant = ['mislav', 'stanko', 'mislav', 'ana', 'mislav'];
+completion = ['stanko', 'ana', 'mislav', 'mislav'];
 output = solution(participant, completion);
 console.log('output : ', output);
